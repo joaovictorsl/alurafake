a9fe9e8f82fb483abe5568c4741ab3016bb3e8cd
João Victor de Souza Lucena <joaovictordev2003@gmail.com>
2025-06-02 03:31:04 -0300

    feat(task): setup task option repository

---
+++ src/main/java/br/com/alura/AluraFake/task/TaskOption.java
@@ -0,0 +1,52 @@
+package br.com.alura.AluraFake.task;
+
+import jakarta.persistence.*;
+
+@Entity
+public class TaskOption {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "task_id", nullable = false)
+    private Task task;
+
+    @Column(nullable = false)
+    private String option;
+
+    @Column(nullable = false)
+    private boolean isCorrect;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Task getTask() {
+        return task;
+    }
+
+    public void setTask(Task task) {
+        this.task = task;
+    }
+
+    public String getOption() {
+        return option;
+    }
+
+    public void setOption(String option) {
+        this.option = option;
+    }
+
+    public boolean isCorrect() {
+        return isCorrect;
+    }
+
+    public void setCorrect(boolean correct) {
+        isCorrect = correct;
+    }
+}
