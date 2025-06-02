34f93935c384cddddd5da2e4a65fea67077d0a27
João Victor de Souza Lucena <joaovictordev2003@gmail.com>
2025-06-02 00:48:38 -0300

    feat(task): use TaskUseCase on TaskController

--- src/main/java/br/com/alura/AluraFake/task/TaskController.java
+++ src/main/java/br/com/alura/AluraFake/task/TaskController.java
@@ -1,60 +1,39 @@
 package br.com.alura.AluraFake.task;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
-import br.com.alura.AluraFake.course.Course;
-import br.com.alura.AluraFake.course.CourseRepository;
-import br.com.alura.AluraFake.course.Status;
-import br.com.alura.AluraFake.util.ErrorItemDTO;
 import jakarta.validation.Valid;
 
 @RestController
 public class TaskController {
 
-    private final TaskRepository taskRepository;
-    private final CourseRepository courseRepository;
+    private final TaskUseCase taskUseCase;
 
     @Autowired
-    public TaskController(TaskRepository taskRepository, CourseRepository courseRepository) {
-        this.taskRepository = taskRepository;
-        this.courseRepository = courseRepository;
+    public TaskController(TaskUseCase taskUseCase) {
+        this.taskUseCase = taskUseCase;
     }
 
     @Transactional
     @PostMapping("/task/new/opentext")
-    public ResponseEntity newOpenTextExercise(@Valid @RequestBody NewOpenTextTaskDTO newOpenTextTaskDTO) {
-        Optional<Course> course = courseRepository.findById(newOpenTextTaskDTO.getCourseId());
-        if (course.isEmpty()) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                    .body(new ErrorItemDTO("courseId", "Curso não encontrado"));
-        } else if (course.get().getStatus() != Status.BUILDING) {
-            return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY)
-                    .body(new ErrorItemDTO("courseId", "Curso não está em construção"));
-        }
-
-        if (taskRepository.existsByStatementAndCourseId(newOpenTextTaskDTO.getStatement(), course.get().getId())) {
-            return ResponseEntity.status(HttpStatus.CONFLICT)
-                    .body(new ErrorItemDTO("statement", "Questão já existe no curso"));
-        }
-
-        taskRepository.save(newOpenTextTaskDTO.toTask(course.get()));
-        return ResponseEntity.status(HttpStatus.CREATED).build();
+    public ResponseEntity<Task> newOpenTextExercise(@Valid @RequestBody NewOpenTextTaskDTO newOpenTextTaskDTO) {
+        Task task = taskUseCase.createOpenTextTask(newOpenTextTaskDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(task);
     }
 
     @PostMapping("/task/new/singlechoice")
-    public ResponseEntity newSingleChoice(@Valid @RequestBody NewSingleChoiceTaskDTO newSingleChoiceTaskDTO) {
-        return ResponseEntity.status(HttpStatus.CREATED).build();
+    public ResponseEntity<Task> newSingleChoice(@Valid @RequestBody NewSingleChoiceTaskDTO newSingleChoiceTaskDTO) {
+        Task task = taskUseCase.createSingleChoiceTask(newSingleChoiceTaskDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(task);
     }
 
     @PostMapping("/task/new/multiplechoice")
-    public ResponseEntity newMultipleChoice(@Valid @RequestBody NewMultipleChoiceTaskDTO newMultipleChoiceTaskDTO) {
-        return ResponseEntity.status(HttpStatus.CREATED).build();
+    public ResponseEntity<Task> newMultipleChoice(@Valid @RequestBody NewMultipleChoiceTaskDTO newMultipleChoiceTaskDTO) {
+        Task task = taskUseCase.createMultipleChoiceTask(newMultipleChoiceTaskDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(task);
     }
-
 }
